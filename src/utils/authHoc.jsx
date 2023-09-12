dbf4e2e293bca81a9e861ca33c1d94b177a8db91
chinweokwu <paulcmorah@gmail.com>
2023-09-12 22:35:02 +0100

    readjust code hoc

--- src/utils/authHoc.jsx
+++ src/utils/authHoc.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/prop-types */
-import { useNavigate } from "react-router-dom";
-// import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 export const RequiredAuth = ({children}) => {
-  const navigate = useNavigate();
-  const token = localStorage.getItem('token')
+  const token = useSelector((state) => state.auth.user)
   const isAuthenticated = !!token;
-  console.log(isAuthenticated)
+  
   if(!isAuthenticated){
-    return navigate("/login")
+    return <Navigate to="/login" />
   }
   return children
 }
