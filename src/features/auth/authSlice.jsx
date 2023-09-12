bf3e042e62e59e541d2b23e51c2dee5fab285a9c
chinweokwu <paulcmorah@gmail.com>
2023-09-12 05:14:42 +0100

    set local storage

--- src/features/auth/authSlice.jsx
+++ src/features/auth/authSlice.jsx
@@ -23,7 +23,6 @@ export const loginUser = createAsyncThunk('auth/loginUser', async (userData, { r
   try {
     const response = await api.post('/login', userData);
     const token = response.data.token;
-    localStorage.setItem('token', token);
     return response.data;
   } catch (error) {
     return rejectWithValue(error.response.data.message);
@@ -45,6 +44,7 @@ const authSlice = createSlice({
         state.loading = false;
         state.isAuthenticated = true;
         if (state.isAuthenticated === true) {
+          localStorage.setItem('token', token);
           toast.success("User registered successfully");
         }
       })
