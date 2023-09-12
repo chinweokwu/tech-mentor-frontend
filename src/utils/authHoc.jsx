73102b93899148a3ce06dbfb054f69cf10e694b2
chinweokwu <paulcmorah@gmail.com>
2023-09-12 22:33:21 +0100

    fix hoc error

--- src/utils/authHoc.jsx
+++ src/utils/authHoc.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 // import { useSelector } from "react-redux";
 export const RequiredAuth = ({children}) => {
   const navigate = useNavigate();
-  const token = console.log(localStorage.getItem('token'))
+  const token = localStorage.getItem('token')
   const isAuthenticated = !!token;
   console.log(isAuthenticated)
   if(!isAuthenticated){
