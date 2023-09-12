b8757ad9bfae1c6f00e7a4996f6e1d39c7d9e826
chinweokwu <paulcmorah@gmail.com>
2023-09-12 13:16:36 +0100

    fix appjs

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const [token, setToken] = useState(null);
-
-  useEffect(() => {
-    const storedToken = localStorage.getItem("token");
-    if (storedToken) {
-      setToken(storedToken);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("token", token);
-  }, [token]);
-
+  const token = localStorage.getItem("token");
   console.log(token);
   const isAuthenticated = !!token;
   console.log(isAuthenticated);
